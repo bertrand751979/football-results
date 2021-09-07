fff79803321a746b6d22ec44c65fe0959477d5ba
bertrand751979 <bertrandassinou@gmail.com>
2021-09-07 19:33:10 +0200

    Small Changes

--- app/src/main/java/com/example/football_score/activity/GroupeOneActivity.java
+++ app/src/main/java/com/example/football_score/activity/GroupeOneActivity.java
@@ -25,7 +25,7 @@ public class GroupeOneActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);;
         setContentView(R.layout.activity_inscription);
         nameOfGroup=findViewById(R.id.groupName);
         team1=findViewById(R.id.counrty1);
